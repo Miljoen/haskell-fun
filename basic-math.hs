b63e3bb0cbeb4c0445955e71c8172bedd49c0e56
Yoeri Joëll Moens <yoerimoens@gmail.com>
2022-10-25 20:48:04 +0200

    Basic math in haskell

---
+++ basic-math.hs
@@ -0,0 +1,12 @@
+main :: IO ()
+
+main = do
+    print (5 + 6.2)
+    print (5 - 6)
+    print (5 * 6)
+    print (5 / 6)
+    print (3 ** 3)
+    print (sqrt 36)
+    print (round 3.4)
+    print (floor 3.9)
+    print (ceiling 3.4)
