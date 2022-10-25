92e7bc3a41948d0c961b34f2434aacd1845ecd35
Yoeri Joëll Moens <yoerimoens@gmail.com>
2022-10-25 21:34:52 +0200

    Cleanup index

--- index.hs
+++ index.hs
@@ -1,21 +1,4 @@
 main :: IO ()
 
-name :: String = "some string"
-
--- myLetter :: Char = 'a'
-
--- number :: Int = 10
-
--- myGpa :: Double = 3.6
-
 main = do
-    putStrLn "Enter first number: "
-    firstString <- getLine
-
-    putStrLn "Enter second number: "
-    secondString <- getLine
-
-    let firstNumber = read firstString::Int
-    let secondNumber = read secondString::Int
-
-    putStrLn (show (firstNumber + secondNumber))
+    putStrLn "index"
