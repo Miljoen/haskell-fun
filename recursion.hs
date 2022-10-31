5546b7caf7f9ce5cdf0418f53af22d85ccf1727a
Yoeri Joëll Moens <yoerimoens@gmail.com>
2022-10-31 22:13:57 +0100

    Added recursion

---
+++ recursion.hs
@@ -0,0 +1,11 @@
+printTo100 :: Int -> IO ()
+printTo100 num = do
+    if num <= 100
+    then do
+        print num
+        printTo100 (num + 1)
+    else putStrLn "Completed the loop!"
+
+main :: IO ()
+main = do
+    printTo100 1
